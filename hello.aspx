ceb8a9506affeac9ccd5e575cb50637a36e14693
Mohammed Fasiulla <mofasiul@microsoft.com>
2021-06-10 18:24:53 -0700

    d

---
+++ hello.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="hello.aspx.cs" Inherits="iram2.hello" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:Button ID="Button1" runat="server" BackColor="#FF3399" OnClick="Button1_Click" Text="Button" />
+        <asp:Button ID="Button2" runat="server" BackColor="#FF3399" OnClick="Button2_Click" Text="SIgnout" />
+        <asp:Button ID="Button3" runat="server" BackColor="#FF3399" OnClick="Button3_Click" Text="additem" />
+         <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+         <asp:Button ID="Button4" runat="server" BackColor="#FF3399" OnClick="Button4_Click" Text="viewitmes" />
+    </form>
+</body>
+</html>
---
+++ hello.aspx.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace iram2
+{
+    public partial class hello : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           // Response.Write("Page load");
+            //HttpCookie userInfo = new HttpCookie("userInfo");
+            //userInfo["UserName"] = "IRAM";            
+            //Response.Cookies.Add(userInfo);
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Write("Hello Worls");
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            //HttpCookie userInfo = new HttpCookie("userInfo");
+            //userInfo["UserName"] = "IRAM";
+            //userInfo.Expires = DateTime.Now.AddDays(-1d);
+            //Response.Cookies.Add(userInfo);
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {            
+            products.additem(TextBox1.Text);
+            Session.Add("product", products.items) ;
+            Response.Write($"Added product {TextBox1.Text} in your shopping cart");
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            var items = Session["product"] as List<string>;
+
+            if(items != null)
+            {
+                Response.Write($"There are {items?.Count} products in your shopping cart");
+                Response.Write("\n");
+                foreach (var item in items)
+                {
+                    Response.Write(item);
+                    Response.Write("\n");
+                }
+            }
+            else
+            {
+                Response.Write($"There are ZERO products in your shopping cart");
+            }
+            
+        }
+    }
+
+    public static class products
+    {
+        public static List<string> items = new List<string>();
+
+        public static void additem(string item)
+        {
+            items.Add(item);
+        }
+    }
+}
